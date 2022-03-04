8e847d8151e91bff82db28a0940d8cc6782c95af
András Horváth <horvath.andras@prevaudit.hu>
2022-03-04 10:26:06 +0100

    init

---
+++ sv2-2021-jvjbf-zarovizsga-potpotvizsga/src/test/java/bistros/AddressTest.java
@@ -0,0 +1,15 @@
+package bistros;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressTest {
+
+    /*@Test
+    void createAddressTest(){
+        Address address = new Address("Kossuth",18);
+
+        assertEquals("Kossuth",address.getStreet());
+        assertEquals(18,address.getNumber());
+    }*/
+}
